a1d3496b1b60c7c26e72d9ac1559f70ac15b6752
akshat_credflow <akshat.aggarwal@credflow.in>
2022-03-05 02:04:13 +0530

    Favicon

--- components/Layout/BaseLayout.jsx
+++ components/Layout/BaseLayout.jsx
@@ -27,7 +27,7 @@ function withBaseLayout(WrappedComponent, props) {
           <meta name="keywords" content={keywordString.replaceAll(' ', ',')} />
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/images/favicon.ico" />
         </Head>
 
         <Navbar fixedNavbar={fixedNavbar} />
